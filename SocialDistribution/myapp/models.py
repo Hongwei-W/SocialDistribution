cec8193f53d04e71388e1c50a26fed3e413da5e8
Darren Wang <darren.wang@farmlinksolutions.ca>
2022-02-10 18:18:54 -0700

    fixed issues with models

--- SocialDistribution/myapp/models.py
+++ SocialDistribution/myapp/models.py
@@ -1,12 +1,9 @@
-from email.policy import default
-from xml.etree.ElementInclude import default_loader
 from django.db import models
-from torch import true_divide
 
 
 # Create your models here.
 class Author(models.Model):
-    type = models.CharField(default="author")
+    type = models.CharField(default="author", max_length=200)
     id = models.CharField(unique=True, max_length=200, primary_key=True)
     host = models.CharField(max_length=200)
     displayName = models.CharField(max_length=200)
@@ -15,63 +12,67 @@ class Author(models.Model):
 
 
 class Authors(models.Model):
-    type = models.CharField(default='authors')
+    type = models.CharField(default='authors', max_length=200)
     items = models.ManyToManyField(to=Author)
 
 
 class Followers(models.Model):
-    type = models.CharField(default='followers')
+    type = models.CharField(default='followers', max_length=200)
     items = models.ManyToManyField(to=Author)
 
 
 class FriendFollowRequest(models.Model):
-    type = models.CharField(default='Follow')
+    type = models.CharField(default='Follow', max_length=200)
     summary = models.TextField()
-    actor = models.ForeignKey(to=Author)
-    object = models.ForeignKey(to=Author)
-
-
-class Post(models.Model):
-    type = models.CharField(default='post')
-    title = models.CharField()
-    id = models.CharField(unique=True, max_length=200, primary_key=True)
-    source = models.Charfield()
-    origin = models.CharField()
-    description = models.TextField()
-    contentType = models.Charfield()
-    author = models.ForeignKey(to=Author)
-    categories = models.TextField()
-    count = models.IntegerField
-    comments = models.CharField()
-    commentsSrc = models.ForeignKey(to=Comment)
-    published = models.DateField()
-    visiblity = models.CharField()
-    unlisted = models.BooleanField()
+    actor = models.ForeignKey(to=Author,
+                              on_delete=models.CASCADE,
+                              related_name='%(class)s_request_sender')
+    object = models.ForeignKey(to=Author,
+                               on_delete=models.CASCADE,
+                               related_name='%(class)s_request_receiver')
 
 
 class Comment(models.Model):
-    type = models.CharField(default='comment')
-    author = models.ForeignKey(to=Author)
+    type = models.CharField(default='comment', max_length=200)
+    author = models.ForeignKey(to=Author, on_delete=models.CASCADE)
     comment = models.TextField()
-    contentType = models.Charfield()
+    contentType = models.CharField(max_length=200)
     published = models.DateField()
     id = models.CharField(unique=True, max_length=200, primary_key=True)
 
 
+class Post(models.Model):
+    type = models.CharField(default='post', max_length=200)
+    title = models.CharField(max_length=200)
+    id = models.CharField(unique=True, max_length=200, primary_key=True)
+    source = models.CharField(max_length=200)
+    origin = models.CharField(max_length=200)
+    description = models.TextField()
+    contentType = models.CharField(max_length=200)
+    author = models.ForeignKey(to=Author, on_delete=models.CASCADE)
+    categories = models.TextField()
+    count = models.IntegerField()
+    comments = models.TextField()
+    commentsSrc = models.ForeignKey(to=Comment, on_delete=models.CASCADE)
+    published = models.DateField()
+    visiblity = models.CharField(max_length=200)
+    unlisted = models.BooleanField()
+
+
 class Like(models.Model):
-    type = models.CharField(default='Like')
+    type = models.CharField(default='Like', max_length=200)
     summary = models.TextField()
-    content = models.CharField()
-    author = models.ForeignKey(to=Author)
-    object = models.ForeignKey(to=Post)
+    content = models.TextField()
+    author = models.ForeignKey(to=Author, on_delete=models.CASCADE)
+    object = models.ForeignKey(to=Post, on_delete=models.CASCADE)
 
 
 class Liked(models.Model):
-    type = models.CharField(default='liked')
+    type = models.CharField(default='liked', max_length=200)
     items = models.ManyToManyField(to=Post)
 
 
 class Inbox(models.Model):
-    type = models.CharField(default='inbox')
-    author = models.ForeignKey(to=Authors)
+    type = models.CharField(default='inbox', max_length=200)
+    author = models.ForeignKey(to=Authors, on_delete=models.CASCADE)
     items = models.ManyToManyField(to=Post)
