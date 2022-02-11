94a990273ad5696fa9024753f36070c896c41d03
Kiana Liu <kianaliu1@gmail.com>
2022-02-10 19:00:59 -0700

    Added urls.py and views.py

--- SocialDistribution/myapp/views.py
+++ SocialDistribution/myapp/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    render(request, 'myapp/login.html')
+def signup(request):
+    render(request, 'myapp/signup.html')
