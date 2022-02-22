5ad16a35cd56a01aaed1e614efb33f618a981e56
Hongwei-W <hongwei_charles_wang@outlook.com>
2022-02-21 20:47:30 -0700

    *NEW [login] add login function (not fully done)

--- SocialDistribution/myapp/urls.py
+++ SocialDistribution/myapp/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'myapp'
 urlpatterns = [
     # ex: /polls/
-    path('login/', views.login, name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
+    # url(r'^logout/$', auth_views.logout, name='logout'),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('feed/', views.feed, name='feed'),
+    # path('', views.LoginView.as_view(), name='login'),
     path('signup/', views.signup, name='signup'),
 
 ]
