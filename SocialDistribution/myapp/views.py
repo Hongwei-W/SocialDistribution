6d7eb90426d78795f357b272716cf63a50964cfe
Hongwei-W <hongwei_charles_wang@outlook.com>
2022-02-23 15:36:32 -0700

    *MDF [login] finish login (todo: show if the password is wrong)

--- SocialDistribution/myapp/views.py
+++ SocialDistribution/myapp/views.py
@@ -5,19 +5,19 @@ from django.shortcuts import render
 from .forms import LoginForm
 
 # Create your views here.
-def login(request):
-    # return HttpResponse("You're looking at login page.")
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
+# def login(request):
+#     # return HttpResponse("You're looking at login page.")
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
 
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            return HTTPResponse(str(username + password))
-    else:
-        form = LoginForm()
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             return HTTPResponse(str(username + password))
+#     else:
+#         form = LoginForm()
 
-    return render(request, 'myapp/login.html', {'form': form})
+#     return render(request, 'myapp/login.html', {'form': form})
 
 def signup(request):
     return HttpResponse("You're looking at signup page.")
