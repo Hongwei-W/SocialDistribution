1bc2e57524fc06d9ec271bccc399facb689af5b6
Darren Wang <darren.wang@farmlinksolutions.ca>
2022-02-24 16:41:23 -0700

    updated colors, added links

--- SocialDistribution/myapp/urls.py
+++ SocialDistribution/myapp/urls.py
@@ -8,12 +8,8 @@ from . import views
 app_name = 'myapp'
 urlpatterns = [
     # ex: /polls/
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
     # url(r'^logout/$', auth_views.logout, name='logout'),
     # path('accounts/', include('django.contrib.auth.urls')),
-    path('feed/', views.feed, name='feed'),
+    # path('feed/', views.feed, name='feed'),
     # path('', views.LoginView.as_view(), name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('feed/', views.feed, name='feed'),
-
 ]
