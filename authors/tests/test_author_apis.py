398ff8013b6326f9d6a17e925767364301c5591a
Kiana Liu <kianaliu1@gmail.com>
2022-04-03 20:57:07 -0600

    added more tests

--- authors/tests/test_author_apis.py
+++ authors/tests/test_author_apis.py
@@ -34,14 +34,14 @@ class TestPostsAPIViews(APITestCase):
 
     def test_AuthorsAPIView_GET(self):
         author = Author.objects.get(username='johnny')
-        print("author found " + author.username)
+        # print("author found " + author.username)
         response = self.client.get(reverse('service-authors'), auth=HTTPBasicAuth("admin", "admin"))
         self.assertEqual(response.status_code,200)
         self.assertIsInstance(response.data['items'], list)
     
     def test_AuthorAPIView_GET(self):
         author = Author.objects.get(username='johnny')
-        print("author found " + author.username)
+        # print("author found " + author.username)
         sample_post = Post(title='testpost', description="This is a new post", content="testpost", contentType="text/plain", unparsedCategories="test", author=author)
         # sample_post = {'title': 'test_post', 'content': 'test_content', 'description': 'test_description', 'content_type': 'text/plain', 'author': 'johnny'}
         sample_post.id = f"{author.host}authors/{str(sample_post.author.uuid)}/posts/{str(sample_post.uuid)}"
@@ -51,7 +51,7 @@ class TestPostsAPIViews(APITestCase):
         # print(serializer.data)
         # response = self.client.post(reverse('service-posts', kwargs={'author':author.uuid}), headers=headerInfo, auth=HTTPBasicAuth("admin", "admin"), data=serializer.data, format='json')
         response = self.client.get(reverse('service-post', kwargs={'pk':sample_post.uuid, 'author':author.uuid}))
-        print(response.data)
+        # print(response.data)
         # print(response.status_code)
         # print(list(response.data['items'][0].items())[1][1])
         self.assertEqual(response.status_code,200)
--- inboxes/tests/test_inbox_apis.py
+++ inboxes/tests/test_inbox_apis.py
@@ -2,15 +2,16 @@ from django.test import Client, TestCase
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from authors.models import Author
-from authors.serializers import *
+from inboxes.models import Inbox, InboxItem
 from posts.models import *
+from authors.serializers import *
 from posts.serializers import *
 import json
 from SocialDistribution import urls
 from requests.auth import HTTPBasicAuth
 
 
-class TestPostsAPIViews(APITestCase):
+class TestInboxAPIViews(APITestCase):
 
     def setUp(self):
         self.c = Client()
@@ -33,44 +34,50 @@ class TestPostsAPIViews(APITestCase):
         self.c.post(url_signup, self.user_info)
         self.c.post(login_url, self.login_correct_info)
 
-    def test_PostsAPIView_GET(self):
+    def test_InboxAPIView_GET(self):
         author = Author.objects.get(username='johnny')
         sample_post = Post(title='testpost', description="This is a new post", content="testpost", contentType="text/plain", unparsedCategories="test", author=author)
         sample_post.id = f"{author.host}authors/{str(sample_post.author.uuid)}/posts/{str(sample_post.uuid)}"
         sample_post.save()
-        response = self.client.get(reverse('service-posts', kwargs={'author':author.uuid}))
+        InboxItem.objects.create(
+            inbox=Inbox.objects.filter(
+                author__username=author.username).first(),
+            inbox_item_type='post',
+            item=sample_post,
+        )
+        
+        response = self.client.get(reverse('service-Inbox', kwargs={'author':author.uuid}))
         self.assertEqual(response.status_code,200)
         self.assertIsInstance(response.data['items'], list)
         self.assertEqual(response.data['items'][0]['title'], sample_post.title)
     
-    def test_PostAPIView_GET(self):
+    def test_InboxAPIView_POST(self):
         author = Author.objects.get(username='johnny')
+        # Test posting a post
         sample_post = Post(title='testpost', description="This is a new post", content="testpost", contentType="text/plain", unparsedCategories="test", author=author)
-        # sample_post = {'title': 'test_post', 'content': 'test_content', 'description': 'test_description', 'content_type': 'text/plain', 'author': 'johnny'}
         sample_post.id = f"{author.host}authors/{str(sample_post.author.uuid)}/posts/{str(sample_post.uuid)}"
         sample_post.save()
-        response = self.client.get(reverse('service-post', kwargs={'pk':sample_post.uuid, 'author':author.uuid}), auth=HTTPBasicAuth("admin", "admin"))
+        serializer = PostSerializer(sample_post)
+        headerInfo = {'Content-Type': 'application/json' }
+        response = self.client.post(reverse('service-Inbox', kwargs={'author':author.uuid}), headers=headerInfo, auth=HTTPBasicAuth("admin", "admin"), data=serializer.data, format='json')
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.data['title'], sample_post.title)
-    
-    def test_CommentsAPIView_GET(self):
-        author = Author.objects.get(username='johnny')
-        sample_post = Post(title='testpost', description="This is a new post", content="testpost", contentType="text/plain", unparsedCategories="test", author=author)
-        # sample_post = {'title': 'test_post', 'content': 'test_content', 'description': 'test_description', 'content_type': 'text/plain', 'author': 'johnny'}
-        sample_post.id = f"{author.host}authors/{str(sample_post.author.uuid)}/posts/{str(sample_post.uuid)}"
-        sample_post.save()
+
+        # Test posting a comment
         sample_comment = Comment(comment="This is a new comment", contentType="text/plain", author=author, post=sample_post)
         sample_comment.id = f"{author.host}authors/{str(sample_comment.author.uuid)}/posts/{str(sample_comment.post.uuid)}/comments/{str(sample_comment.uuid)}"
         sample_comment.save()
-        response = self.client.get(reverse('service-comments', kwargs={'post':sample_post.uuid, 'author':author.uuid}), auth=HTTPBasicAuth("admin", "admin"))
+        serializer = CommentsSerializer(sample_comment)
+        headerInfo = {'Content-Type': 'application/json' }
+        response = self.client.post(reverse('service-Inbox', kwargs={'author':author.uuid}), headers=headerInfo, auth=HTTPBasicAuth("admin", "admin"), data=serializer.data, format='json')
         self.assertEqual(response.status_code,200)
-        self.assertEqual(response.data['items'][0]['comment'], sample_comment.comment)
-    
-    def test_CommentsAPIView_POST(self):
-        author = Author.objects.get(username='johnny')
-        sample_post = Post(title='testpost', description="This is a new post", content="testpost", contentType="text/plain", unparsedCategories="test", author=author)
-        # sample_post = {'title': 'test_post', 'content': 'test_content', 'description': 'test_description', 'content_type': 'text/plain', 'author': 'johnny'}
-        sample_post.id = f"{author.host}authors/{str(sample_post.author.uuid)}/posts/{str(sample_post.uuid)}"
-        sample_post.save()
-        response = self.client.post(reverse('service-comments', kwargs={'post':sample_post.uuid, 'author':author.uuid}), auth=HTTPBasicAuth("admin", "admin"), data={"comment":"This is a new comment", "contentType":"text/plain", "author":author.uuid, "object":sample_post.id})
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.data['comment'], sample_comment.comment)
+
+        # # Test posting a like
+        # sample_like = Like(author=author, object=sample_post.id)
+        # sample_like.save()
+        # serializer = LikesSerializer(sample_like)
+        # headerInfo = {'Content-Type': 'application/json' }
+        # response = self.client.post(reverse('service-Inbox', kwargs={'author':author.uuid}), headers=headerInfo, auth=HTTPBasicAuth("admin", "admin"), data=serializer.data, format='json')
+        # # print("response data: ", response.data)
+        # self.assertEqual(response.status_code,200)
