cfe500bf20cf587fed9271a669c0c388a139e63e
Kiana Liu <kianaliu1@gmail.com>
2022-04-03 20:58:15 -0600

    edited localHostlist

--- common/views.py
+++ common/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 localHostList = [
-    'https://c404-social-distribution.herokuapp.com/'
+    'https://c404-social-distribution.herokuapp.com/',
+    'http://localhost:8000/',
+    'http://127.0.0.1:8000/'
 ]
